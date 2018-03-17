92f51d51da8264bef54bb3d0ea8427fd0fb706de
上垣正顕 <uwettie@uegakimasaakinoiMac.local>
2018-03-17 11:20:28 +0900

    to section 57(https://www.udemy.com/php7study/)

---
+++ src/routes/web.php
@@ -0,0 +1,38 @@
+<?php
+use App\Book;
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('books');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::group(['middleware' => ['web']], function() {
+    Route::get('/', function() {
+        //
+    });
+
+    Route::post('/book', function(Request $request) {
+        //
+    });
+
+    Route::delete('/book/{book}', function(Book $book) {
+        //
+    });
+});
