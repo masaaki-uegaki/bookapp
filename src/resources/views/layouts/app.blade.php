84d6299605ef233430d6393741f03c986e03a9e5
上垣正顕 <uwettie@uegakimasaakinoiMac.local>
2018-03-18 16:32:08 +0900

    Localization

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', '書籍リスト') }}</title>
+    <title>{{ config('app.name', __('書籍リスト')) }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
